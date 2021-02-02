c1d4ecd72636c4abff62ca3a2dc462a44bf0d612
Vivek <vivektheintel@gmail.com>
2021-02-03 00:07:00 +0530

    Calc service - init

---
+++ src/test/java/com/vv/personal/twm/calc/TwmCalcServerTests.java
@@ -0,0 +1,15 @@
+package com.vv.personal.twm.calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/*
+@SpringBootTest
+class TwmCalcServerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
